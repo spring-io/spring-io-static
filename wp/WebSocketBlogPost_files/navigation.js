2c2dbb10c235f4547c1905b5f377ce2e353904df
Trevor Marshall <tmarshall@vmware.com>
2025-03-05 09:15:47 -0800

    Move ancient blog.springsource.com static content from assets.spring.io S3 bucket to static.spring.io GCP bucket

---
+++ wp/WebSocketBlogPost_files/navigation.js
@@ -0,0 +1,39 @@
+function mouseOver1()
+{
+document.getElementById("b1").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_features_hover.png"
+}
+
+function mouseOut1()
+{
+document.getElementById("b1").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_features.png"
+}
+
+function mouseOver2()
+{
+document.getElementById("b2").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_getstarted_hover.png"
+}
+
+function mouseOut2()
+{
+document.getElementById("b2").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_getstarted.png"
+}
+
+function mouseOver3()
+{
+document.getElementById("b3").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_getinvolved_hover.png"
+}
+
+function mouseOut3()
+{
+document.getElementById("b3").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_getinvolved.png"
+}
+
+function mouseOver4()
+{
+document.getElementById("b4").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_newsevents_hover.png"
+}
+
+function mouseOut4()
+{
+document.getElementById("b4").src ="http://www.springsource.org/sites/all/themes/s2org11/images/nav_newsevents.png"
+}
---
+++ wp/WebSocketBlogPost_files/pingpop.js
@@ -0,0 +1,8 @@
+function pingsOn() {
+	document.getElementById("pingslist").style.display = "";
+	document.getElementById("pinglistoff").style.display = "none";
+}
+function pingsOff() {
+	document.getElementById("pingslist").style.display = "none";
+	document.getElementById("pinglistoff").style.display = "";
+}
